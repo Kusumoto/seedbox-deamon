ef924ced70974c26c7fcfb5c041a8ab6ea58dd80
Weerayut <weerayut.h@iconext.co.th>
2017-03-29 10:35:06 +0700

    add transmission container control.

--- services/transmissions/transmission_pull.go
+++ services/transmissions/transmission_pull.go
@@ -1,1 +1,16 @@
 package transmissions
+
+import (
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+// PullTransmissionImage pull a lasted transmission image from repository
+func PullTransmissionImage(cli *docker.Client) error {
+	// Setting image option
+	var imageOption = docker.PullImageOptions{
+		Registry: TransmissionImageName,
+		Tag:      TransmissionImageTag,
+	}
+	// Pull image
+	return cli.PullImage(imageOption, docker.AuthConfiguration{})
+}
--- services/transmissions/transmission_remove.go
+++ services/transmissions/transmission_remove.go
@@ -1,2 +1,13 @@
 package transmissions
 
+import docker "github.com/fsouza/go-dockerclient"
+
+// RemoveTransmissionContainer remove a transmission container
+func RemoveTransmissionContainer(cli *docker.Client, config *TransmissionConfig) error {
+	// Setting container option
+	var removeContainerOption = docker.RemoveContainerOptions{
+		ID: config.ContainerID,
+	}
+	// Remove container
+	return cli.RemoveContainer(removeContainerOption)
+}
