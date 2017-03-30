dbada36558cd13a7afc90eb165070b071be483d7
Weerayut <weerayut.h@iconext.co.th>
2017-03-30 18:19:41 +0700

    add docker client comment

--- api/client.go
+++ api/client.go
@@ -2,6 +2,7 @@ package api
 
 import docker "github.com/fsouza/go-dockerclient"
 
+// NewDockerClient is create a new docker instant
 func NewDockerClient() (docker.Client, error) {
 	client, err := docker.NewClient(defaultDockerEndpoint)
 	return *client, err
